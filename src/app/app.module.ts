9ec67888194ffb25b3c369b9a41d93a097e090e3
ojambo <ojambo04@gmail.com>
2019-12-13 13:19:39 +0530

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,13 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { NgModule } from "@angular/core";
+import { FlexLayoutModule } from "@angular/flex-layout";
+import { MatCardModule } from "@angular/material/card";
+import { BrowserModule } from "@angular/platform-browser";
+import { AppComponent } from "./app.component";
+import { FileExplorerModule } from "./file-explorer/file-explorer.module";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
+  declarations: [AppComponent],
+  imports: [BrowserModule, FileExplorerModule, FlexLayoutModule, MatCardModule],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/file-explorer/file-explorer.module.ts
@@ -0,0 +1,39 @@
+import { CommonModule } from "@angular/common";
+import { NgModule } from "@angular/core";
+import { FlexLayoutModule } from "@angular/flex-layout";
+import { FormsModule } from "@angular/forms";
+import { MatButtonModule } from "@angular/material/button";
+import { MatDialogModule } from "@angular/material/dialog";
+import { MatGridListModule } from "@angular/material/grid-list";
+import { MatIconModule } from "@angular/material/icon";
+import { MatInputModule } from "@angular/material/input";
+import { MatMenuModule } from "@angular/material/menu";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { FileExplorerComponent } from "./file-explorer/file-explorer.component";
+import { NewFolderDialogComponent } from "./modals/new-folder-dialog/new-folder-dialog.component";
+import { RenameDialogComponent } from "./modals/rename-dialog/rename-dialog.component";
+
+@NgModule({
+  imports: [
+    CommonModule,
+    MatToolbarModule,
+    FlexLayoutModule,
+    MatIconModule,
+    MatGridListModule,
+    MatMenuModule,
+    BrowserAnimationsModule,
+    MatDialogModule,
+    MatInputModule,
+    FormsModule,
+    MatButtonModule
+  ],
+  declarations: [
+    FileExplorerComponent,
+    NewFolderDialogComponent,
+    RenameDialogComponent
+  ],
+  entryComponents: [NewFolderDialogComponent, RenameDialogComponent],
+  exports: [FileExplorerComponent]
+})
+export class FileExplorerModule {}
